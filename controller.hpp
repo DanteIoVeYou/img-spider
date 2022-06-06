429415cde36bac58518c845056395b0c2740aa20
danteking <danteking@aliyun.com>
2022-06-06 23:06:25 +0800

    downloader done

--- controller.hpp
+++ controller.hpp
@@ -2,6 +2,7 @@
 #include "utils.hpp"
 #include "epoll.hpp"
 #include "http.hpp"
+#include "downloader.hpp"
 // #include "config.hpp"
 // #include "downloader.hpp"
 // #include "parser.hpp"
@@ -11,6 +12,7 @@ class Controller
 public:
     Controller(Url seed) : _seed(seed)
     {
+        // 输入URL种子
         _url_map.insert({_seed._domain + _seed._path, _seed});
         _url_wait_queue.push(_seed);
     }
@@ -31,21 +33,53 @@ public:
         //     _epoll.Dispatch(10, 1000, _page_storage);
         //     std::cout << _page_storage.front() << std::endl;
         // }
+
+        // while (true)
+        // {
+        //     if (!_url_wait_queue.empty())
+        //     {
+        //         Url url = _url_wait_queue.front();
+        //         _url_wait_queue.pop();
+        //         std::string ip = url._ip;
+        //         std::string domain = url._domain;
+        //         std::string path = url._path;
+        //         Http http(ip, domain, &_epoll);
+        //         _epoll.Add(http, EPOLLOUT | EPOLLET); // 添加写事件
+        //     }
+        //     else
+        //     {
+        //         int n = _epoll.Dispatch(10, 3000, _page_storage);
+        //         if (n == 0)
+        //         {
+        //             std::cout << "all ev resolved.." << std::endl;
+        //             break;
+        //         }
+        //         else
+        //         {
+        //             // for (auto &e : _page_storage)
+        //             // {
+        //             //     std::cout << e << std::endl;
+        //             // }
+        //             std::cout << "n: " << n << std::endl;
+        //         }
+        //     }
+        // }
+
         while (true)
         {
             if (!_url_wait_queue.empty())
             {
+                // 取出等待队列中的一个待抓取的URL
                 Url url = _url_wait_queue.front();
                 _url_wait_queue.pop();
-                std::string ip = url._ip;
-                std::string domain = url._domain;
-                std::string path = url._path;
-                Http http(ip, domain, &_epoll);
-                _epoll.Add(http, EPOLLOUT | EPOLLET); // 添加写事件
+                // 调度一个下载器进行下载
+                Downloader downloader(url, &_epoll);
+                downloader.PreDownload();
             }
             else
             {
-                int n = _epoll.Dispatch(10, 3000, _page_storage);
+                // int n = downloader.Download(10, 2000, _page_storage);
+                int n = _epoll.Dispatch(10, 100, _page_storage);
                 if (n == 0)
                 {
                     std::cout << "all ev resolved.." << std::endl;
@@ -53,10 +87,10 @@ public:
                 }
                 else
                 {
-                    // for (auto &e : _page_storage)
-                    // {
-                    //     std::cout << e << std::endl;
-                    // }
+                    for (auto &e : _page_storage)
+                    {
+                        std::cout << e << std::endl;
+                    }
                     std::cout << "n: " << n << std::endl;
                 }
             }
@@ -75,10 +109,10 @@ public:
 private:
     Epoll _epoll;
     // Config _config;         // 配置文件模块
-    // Downloader _downloader; // 下载模块
+    // std::queue<Downloader> _downloaders; // 下载模块
     // Parser _parser;         // 页面处理模块
     Url _seed;
     std::unordered_map<std::string, Url> _url_map; // 已经抓取和待抓取的所有URL的集合
     std::queue<Url> _url_wait_queue;               // 未抓取的URL放入等待队列
-    std::vector<std::string> _page_storage;
+    std::vector<std::string> _page_storage;        // 保存已经获取的响应的页面的数组
 };
--- downloader.hpp
+++ downloader.hpp
@@ -1,12 +1,29 @@
 #pragma once
 
 #include "http.hpp"
-
+#include "utils.hpp"
+// #include "epoll.hpp"
+class Epoll;
 class Downloader
 {
 public:
-    Downloader() {}
+    Downloader(const Url &url, Epoll *ep) : _url(url), _r(ep), _http(_url._ip, _url._domain, _r) {}
+
+    void PreDownload()
+    {
+        _r->Add(_http, EPOLLOUT | EPOLLET); // 添加写事件
+    }
+
+    // int Download(int num, int timeout, std::vector<std::string> &storage)
+    // {
+    //     return _r->Dispatch(num, timeout, storage);
+    // }
     ~Downloader() {}
-    
+
 private:
+    Url _url;
+
+public:
+    Epoll *_r;
+    Http _http;
 };
